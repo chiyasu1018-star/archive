555fa5f01de0129b93a3aaf21f241247920a2c6f
chiyasu1018-star <chiyasu1018@gmail.com>
2026-03-24 11:36:42 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,15 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: './',
+  // 如果你的 GitHub 地址是 https://用户名.github.io/archive/
+  // 那么这里就填 '/archive/'
+  base: '/archive/', 
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
